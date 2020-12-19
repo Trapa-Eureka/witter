8d8b8e161f540944e41106eb86381eac1ddb7572
JINHO SON <a1234@1234s-MacBook-Pro.local>
2020-12-19 18:58:01 +0900

    1.2 Secure keys

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,12 @@
 import firebase from "firebase/app";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyB83-dHoTld0CVXd8a2xk0A2HIpJMMi-1A",
-    authDomain: "witter-a2b5a.firebaseapp.com",
-    projectId: "witter-a2b5a",
-    storageBucket: "witter-a2b5a.appspot.com",
-    messagingSenderId: "539750756356",
-    appId: "1:539750756356:web:015ed3d89f0f0db9c14551"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGIN_ID,
+    appId: process.env.REACT_APP_APP_ID
 };
 
 export default firebase.initializeApp(firebaseConfig);
