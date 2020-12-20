59021333f2c79b4a440b853ff2b14ad781b01599
JINHO SON <a1234@1234s-MacBook-Pro.local>
2020-12-20 16:13:35 +0900

    3.1 form & database Setting

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,4 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Home = () => <span>Home</span>;
+const Home = () => {
+    const [post, setPost] = useState("");
+    const onSubmit = (event) => {
+        event.preventDefault();
+    };
+    const onChange = (event) => {
+        const { 
+            target: { value },
+        } = event;
+        setPost(value);
+    };
+    return (
+        <div>
+            <form onSubmit={onSubmit}>
+                <input value={post} onChange={onChange} type="text" placeholder="What is your plan on Today?" maxLength={120} />
+                <input type="submit" value="Post" />
+            </form>
+        </div>
+    );
+};
+
 export default Home;
