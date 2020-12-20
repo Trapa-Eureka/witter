1ab853ef0916d96937edca2d70b2130f94a5abff
JINHO SON <a1234@1234s-MacBook-Pro.local>
2020-12-20 21:10:56 +0900

    3.6 delete & updating 2

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,15 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
+import { dbService } from "fbase";
 
-const Post = ({ postObj, isOwner }) => (
-    <div>
-        <h4>{postObj.text}</h4>
-        {isOwner && (
-            <>
-                <button>Delete Post</button>
-                <button>Edit Post</button>
-            </>
-        )}
-    </div>
-);
+const Post = ({ postObj, isOwner }) => {
+    const [editing, setEditing] = useState(false);
+    const [newPost, setNewPost] = useState(postObj.text);
+    const onDeleteClick = async () => {
+        const ok = window.confirm("Are you sure you want to delete this post?");
+        console.log(ok);
+        if (ok) {
+            await dbService.doc(`posts/${postObj.id}`).delete();
+        }
+    };
+    const toggleEditing = () => setEditing((prev) => !prev);
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        await dbService.doc(`posts/${postObj.id}`).update({
+            text: newPost,
+        });
+        setEditing(false);
+    };
+    const onChange = (event) => {
+        const {target: { value },} = event;
+        setNewPost(value);
+    };
+    return (
+        <div>
+            {editing ? (
+                <>
+                    <form onSubmit={onSubmit}>
+                        <input type="text" placeholder="Edit your post" value={newPost} required onChange={onChange} />
+                        <input type="submit" value="Update Post" />
+                    </form>
+                    <button onClick={toggleEditing}>Cancel</button>
+                </>
+            ) : (
+                <>
+                    <h4>{postObj.text}</h4>
+                    {isOwner && (
+                        <>
+                            <button onClick={onDeleteClick}>Delete Post</button>
+                            <button onClick={toggleEditing}>Edit Post</button>
+                        </>
+                    )}
+                </>
+            )}
+        </div>
+    );
+};
 
 export default Post;
