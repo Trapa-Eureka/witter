ebadea39d62d29f9860a2d049789604b7cabd2df
JINHO SON <a1234@1234s-MacBook-Pro.local>
2020-12-20 18:10:32 +0900

    3.3 Posting on Screen & CR of CRUD

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,8 +1,22 @@
 import { dbService } from "fbase";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const Home = () => {
     const [post, setPost] = useState("");
+    const [posts, setPosts] = useState([]);
+    const getPosts = async() => {
+        const dbPosts = await dbService.collection("posts").get();
+        dbPosts.forEach((document) => {
+            const postObject = {
+                ...document.data(),
+                id: document.id,
+            };
+            setPosts((prev) => [postObject, ...prev]);
+        });
+    };
+    useEffect (() => {
+        getPosts();
+    }, []);
     const onSubmit = async(event) => {
         event.preventDefault();
         await dbService.collection("posts").add({
@@ -17,12 +31,20 @@ const Home = () => {
         } = event;
         setPost(value);
     };
+    console.log(posts);
     return (
         <div>
             <form onSubmit={onSubmit}>
                 <input value={post} onChange={onChange} type="text" placeholder="What is your plan on Today?" maxLength={120} />
                 <input type="submit" value="Post" />
             </form>
+            <div>
+                {posts.map(post => 
+                    <div key={post.id}>
+                        <h4>{post.post}</h4>
+                    </div>
+                )}
+            </div>
         </div>
     );
 };
