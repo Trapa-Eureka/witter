fc3b18137a40bc506762b4537c614f4ff8eeaefb
JINHO SON <a1234@1234s-MacBook-Pro.local>
2020-12-20 18:56:35 +0900

    3.4 Realtime Posting with onSnapshot

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,27 +1,25 @@
 import { dbService } from "fbase";
 import React, { useEffect, useState } from "react";
 
-const Home = () => {
+const Home = ({ userObj }) => {
+    console.log(userObj);
     const [post, setPost] = useState("");
     const [posts, setPosts] = useState([]);
-    const getPosts = async() => {
-        const dbPosts = await dbService.collection("posts").get();
-        dbPosts.forEach((document) => {
-            const postObject = {
-                ...document.data(),
-                id: document.id,
-            };
-            setPosts((prev) => [postObject, ...prev]);
-        });
-    };
     useEffect (() => {
-        getPosts();
+        dbService.collection("posts").onSnapshot(snapshot => {
+            const postArray = snapshot.docs.map(doc => ({
+                id: doc.id,
+                ...doc.data(),
+            }));
+            setPosts(postArray);
+        });
     }, []);
     const onSubmit = async(event) => {
         event.preventDefault();
         await dbService.collection("posts").add({
-            post,
+            text: post,
             createAt: Date.now(),
+            creatorId: userObj.uid,
         });
         setPost("");
     };
@@ -31,7 +29,6 @@ const Home = () => {
         } = event;
         setPost(value);
     };
-    console.log(posts);
     return (
         <div>
             <form onSubmit={onSubmit}>
@@ -41,7 +38,7 @@ const Home = () => {
             <div>
                 {posts.map(post => 
                     <div key={post.id}>
-                        <h4>{post.post}</h4>
+                        <h4>{post.text}</h4>
                     </div>
                 )}
             </div>
